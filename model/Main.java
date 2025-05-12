c05d7842672c8e0142af033405ade89dc11575f6
Shadowhunterbr <luiselaisdijonase@gmail.com>
2025-05-12 15:44:40 -0300

    First Commit

---
+++ model/Main.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Iphone meuIPhone = new Iphone();
+	        meuIPhone.tocar();
+	        meuIPhone.selecionarMusica("Minha Música");
+	        meuIPhone.ligar("123-456-7890");
+	        meuIPhone.exibirPagina("https://example.com");
+
+	}
+
+}
